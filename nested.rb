9a376e94bc2265bd815a56edf0c8265451b3886f
Mark Stabler <mark@markstabler.com>
2020-03-10 00:20:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,6 +78,12 @@ def adding_matz
         }
      }
 
+		 # add the following information to the top level of programmer_hash
+		 # :yukihiro_matsumoto => {
+		 #   :known_for => "Ruby",
+		 #    :languages => ["LISP", "C"]
+		 # }
+
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
