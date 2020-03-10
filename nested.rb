d77f9decd5fbe46438c91fb570b586ab779a7764
Mark Stabler <mark@markstabler.com>
2020-03-10 00:16:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,12 +62,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -84,7 +78,7 @@ def adding_matz
         }
      }
 
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
